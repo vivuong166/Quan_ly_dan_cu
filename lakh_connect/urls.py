f49372c675839d46c3a39592e4cda6f63440d4b7
KVan11 <nguyenvan2005ty@gmail.com>
2025-12-26 00:36:43 +0700

    cập nhật 404

--- lakh_connect/urls.py
+++ lakh_connect/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.urls import path, re_path
+from core.views import page_not_found
 urlpatterns = [
     path('admin/', admin.site.urls),
     # expose core views at the project root so URLs like `/` and `/home/` work
     path('', include('core.urls')),
     # also keep the API under /api/
+    re_path(r"^.*$", page_not_found),
 ]
