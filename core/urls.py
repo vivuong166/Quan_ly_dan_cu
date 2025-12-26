23b556b44f71ea193cc8b1394d455d65eb88caa3
Ducanh2005HUST <ducanheya@gmail.com>
2025-12-26 20:40:12 +0700

    trang mặc định là login

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # =====================
     # AUTH
     # =====================
-    path("login/", views.login_view, name="login"),
+    path("", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("home/", views.home, name="home"),
 
